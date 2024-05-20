d084a23abe119ceb84f6df98253e4806fc6e6154
khayrullayev2006 <ogabekxayrullayev240@gmail.com>
2024-05-20 15:18:03 +0500

    server created

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.write(JSON.stringify({ message: "Hello World" }));
+  res.end()
+});
+
+server.listen(3000, () => {
+  console.log("Server has started on PORT:3000");
+});
