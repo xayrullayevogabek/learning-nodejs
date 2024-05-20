ca825238ec49d36eff282b391de2e4ff1fea9d7b
khayrullayev2006 <ogabekxayrullayev240@gmail.com>
2024-05-20 13:35:37 +0500

    learning nodejs

---
+++ modules/path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+// console.log(path.basename(__filename)) turgan file nomi
+// console.log(path.basename(__dirname)) tugan papka nomi
+// console.log(path.extname(__filename)) turgan file kengaytma nomi
+// console.log(path.parse(__filename)) filega oid malumotlarni objectni ichida beradi
+// console.log(path.join(__dirname, 'object', 'index.js')); faylarni ulash
+// console.log(path.resolve(__dirname, 'object', 'index.js')) tepadagi bilan pachti bir xil
