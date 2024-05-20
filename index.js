880e9a0af9d71931da2b1662a0edb5b9f5ac7830
khayrullayev2006 <ogabekxayrullayev240@gmail.com>
2024-05-20 16:03:19 +0500

    web-server

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <h2>Send Your Name</h2>
+        <form action="/" method="POST">
+            <input type="text" name="name" />
+            <button type="submit">Send Name</button>
+        </form>
+    `);
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    const body = [];
 
-  res.write(JSON.stringify({ message: "Hello World" }));
-  res.end();
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+
+      res.end(`<h2>Hello ${message}</h2>`);
+    });
+  }
 });
 
 server.listen(3000, () => {
