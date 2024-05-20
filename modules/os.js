b6582dae5cd125996c635858c393af52cab15008
khayrullayev2006 <ogabekxayrullayev240@gmail.com>
2024-05-20 14:37:36 +0500

    os module added

---
+++ modules/os.js
@@ -0,0 +1,34 @@
+const os = require("os");
+
+// System architecture
+// console.log(os.arch())
+
+// System CPU
+// console.log(os.cpus())
+
+// System free memory
+// console.log(os.freemem());
+
+// System root directory
+// console.log(os.homedir());
+
+// System total memory
+// console.log(os.totalmem());
+
+// System network information
+// console.log(os.networkInterfaces());
+
+// System platform
+// console.log(os.platform());
+
+// System user information
+// console.log(os.userInfo());
+
+// System type
+// console.log(os.type());
+
+// System Version
+// console.log(os.version());
+
+// System Temp file path
+// console.log(os.tmpdir());
